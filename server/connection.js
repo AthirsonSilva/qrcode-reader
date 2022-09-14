30427cd91371e77bef4e68c0e05bd62d8126deeb
Athirson <athirsonarceus@gmail.com>
2022-09-14 19:20:16 -0300

    server something

---
+++ server/connection.js
@@ -0,0 +1,50 @@
+const mysqlConnection = (qrData, qrType) => {
+  const mysql = require('mysql');
+
+  const connection = mysql.createConnection({
+      host: '127.0.0.1',
+      user: 'root',
+      password: '!Potter4',
+      database: 'scans'
+  })
+  
+  const sendScan = connection.connect((err, qrData, qrType) => {
+      if (err) throw err;
+      console.log('connected!');
+  
+      let query = `INSERT INTO Scans (data, type) VALUES (${qrData}, ${qrType})`
+  
+      connection.query(query, (err, result) => {
+        if (err) throw err;
+  
+        console.log(`result: ${result}`);
+        return true
+      })
+  }) 
+}
+
+const mssqlConnection = (err, qrData, qrType) => {
+  const mssql = require('mssql')
+
+  const config = {
+    user: 'sa',
+    password: '@Potter4',
+    server: '127.0.0.1',
+    database: 'scans'
+  }
+
+  mssql.connect(config, err => {
+    if (err) throw err
+
+    const request = new mssql.Request()
+
+    request.query(`INSERT INTO scans (data, type) VALUES (${qrData}, ${qrType})`, (err, result) => {
+      if (err) throw err
+
+      console.log(result)
+      return true
+    })
+  }, qrData, qrType)
+}
+
+export { mssqlConnection, mysqlConnection }
--- server/server.js
+++ server/server.js
@@ -1,50 +1,9 @@
-/* const mysqlConnection = (qrData, qrType) => {
-  const mysql = require('mysql');
+require('dotenv').config()
 
-  const connection = mysql.createConnection({
-      host: '127.0.0.1',
-      user: 'root',
-      password: '!Potter4',
-      database: 'scans'
-  })
-  
-  const sendScan = connection.connect((err, qrData, qrType) => {
-      if (err) throw err;
-      console.log('connected!');
-  
-      let query = `INSERT INTO Scans (data, type) VALUES (${qrData}, ${qrType})`
-  
-      connection.query(query, (err, result) => {
-        if (err) throw err;
-  
-        console.log(`result: ${result}`);
-        return true
-      })
-  }) 
-} */
+const express = require('express')
+const app = express()
+const connection = require('./connection/mysqlConnection')
 
-const mssqlConnection = (err, qrData, qrType) => {
-  const mssql = require('mssql')
+const PORT = process.env.PORT || 3000
 
-  const config = {
-    user: 'sa',
-    password: '@Potter4',
-    server: '127.0.0.1',
-    database: 'scans'
-  }
-
-  mssql.connect(config, err => {
-    if (err) throw err
-
-    const request = new mssql.Request()
-
-    request.query(`INSERT INTO scans (data, type) VALUES (${qrData}, ${qrType})`, (err, result) => {
-      if (err) throw err
-
-      console.log(result)
-      return true
-    })
-  }, qrData, qrType)
-}
-
-export default mssqlConnection
+// app.post('/post')
