5c10f75ffc89656c19a131938aaeaa5746fb93ee
Athirson <athirsonarceus@gmail.com>
2022-10-02 20:33:28 -0300

    setData() not working still

--- screens/RestrictedPage.tsx
+++ screens/RestrictedPage.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import { FlatList, Text, View, ActivityIndicator } from 'react-native';
+import { Table, TableWrapper, Row, Rows } from 'react-native-table-component';
+
+
 import styles from '../styles';
 
 export default function RestrictedPage() {
     const [data, setData] = React.useState([])
+    const [headers, setHeaders] = React.useState([])
     const [loading, setLoading] = React.useState(true)
 
     const getAllScans = async () => {
@@ -18,10 +22,10 @@ export default function RestrictedPage() {
         )
         .then((response) => response.json())
         .then((json) => {
-            setData(json)
+            setData(json.scans)
 
             console.log('FETCHED DATA')
-            console.table(json.scans)
+            console.table(data)
         })
         .catch((error) => console.error(error))
         .finally(() => setLoading(false));
@@ -29,21 +33,26 @@ export default function RestrictedPage() {
 
     React.useEffect(() => {
         getAllScans()
+        setHeaders(['ID', 'Data', 'Data de criação', 'Data de atualização'])
     }, [])
     
     return (
         <View style={[ styles.container, { backgroundColor: '#fff' } ]}>
              {loading ? <ActivityIndicator/> : (
-                <FlatList
-                    data={data}
-                    keyExtractor={({ ID }, index) => ID}
-                    renderItem={({ item }) => (
-                    <View style={[ styles.container ]}>
-                        <Text>{item.ID} - {item.qrData}</Text>
-                    </View>
-                    )}
-                />
-                )}            
+                <View style={styles.container}>
+                    <TableWrapper style={styles.container}>
+                        <Table borderStyle={{borderWidth: 2, borderColor: '#c8e1ff'}}>
+                            <Row data={headers} style={styles.headStyle} textStyle={styles.headerText}/>
+                            <Rows data={[
+                                ['1', '2', '3', 'a'],
+                                ['a', 'b', 'c', '1'],
+                                ['1', '2', '3', 'a'],
+                            ]} 
+                            textStyle={styles.rowText}/>
+                        </Table>
+                    </TableWrapper>
+                </View>
+                )}   
         </View>
     );
 }
